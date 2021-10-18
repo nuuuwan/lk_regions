a0513575dec9bfc3e0c4dcc6ec9985e18bac995d
nuuuwan <nuuuwan@gmail.com>
2021-10-18 16:48:09 +0530

    Copied Data from gig-data

--- scripts/copy-data.sh
+++ scripts/copy-data.sh
@@ -1,3 +1,7 @@
 DIR_GIG_DATA=/Users/nuwan.senaratna/Not.Dropbox/_CODING/data/gig-data
 
 cp -r $DIR_GIG_DATA/* public/data/gig-data/
+
+git add .
+git commit -m "Copied Data from gig-data"
+git push origin main
